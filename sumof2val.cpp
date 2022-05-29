9a2ce6f8361df8a3a7cd40cabd7401c0b39baa8a
Tanishq Rajput <ertanishqrajput@gmail.com>
2022-05-30 01:45:04 +0530

    SS

---
+++ sumof2val.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define mod 1000000007
+#define mp make_pair
+#define pb(x) push_back(x)
+#define popb pop_back()
+#define ln "\n"
+#define ar array
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+int main()
+{
+   
+}
