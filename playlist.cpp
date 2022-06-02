1304dada3053fe3116c5b39633d973b49776f134
Tanishq Rajput <ertanishqrajput@gmail.com>
2022-06-02 18:01:25 +0530

    SS

---
+++ playlist.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define mod 1000000007
+#define mp make_pair
+#define pb(x) push_back(x)
+#define popb pop_back()
+#define ln "\n"
+#define ar array
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+int main()
+{
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    map<int,int> s;
+    int i=0,j=0;int ans=1;
+    while(i<n)
+    {
+        while(j<n&&s[a[j]]!=1)
+        {
+            s[a[j]]++;
+            j++;
+        }
+        ans=max(ans,j-i);
+        s[a[i]]--;
+        i++;
+    }
+    cout<<ans;
+}
