80158a16ad7fc7cdaa9bfd2a2198f8a1980bf2bb
Tanishq Rajput <ertanishqrajput@gmail.com>
2022-06-04 00:33:38 +0530

    SS

---
+++ reading_books.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define mod 1000000007
+#define mp make_pair
+#define pb(x) push_back(x)
+#define popb pop_back()
+#define ln "\n"
+#define ar array
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+const int mxn=2e5;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    ll sum=0;int m=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        m=max(m,temp);
+        sum+=temp;
+
+    }
+    if(m>sum-m)
+    cout<<2*m;
+    else
+    cout<<sum;
+}
